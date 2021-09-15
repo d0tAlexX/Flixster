bc1a42c5b54527ad010e41529bd6717040d8295e
home <dot.AlexX@gmail.com>
2021-09-15 03:30:59 -0400

    FLixster App Part 2 Commit

--- app/src/main/java/com/codepath/dotalexx/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/dotalexx/flixster/adapters/MovieAdapter.java
@@ -1,18 +1,24 @@
 package com.codepath.dotalexx.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
+import com.codepath.dotalexx.flixster.DetailActivity;
 import com.codepath.dotalexx.flixster.R;
 import com.codepath.dotalexx.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -47,12 +53,11 @@ public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.ViewHolder>
 
     //Return total count of items in the list
     @Override
-    public int getItemCount() {
-        return 0;
-    }
+    public int getItemCount() { return movies.size(); }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+            RelativeLayout container;
             TextView tvTitle;
             TextView tvOverview;
             ImageView ivPoster;
@@ -62,6 +67,7 @@ public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.ViewHolder>
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
 
 
         }
@@ -70,6 +76,21 @@ public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.ViewHolder>
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+
+            //Refgister click listener on the whole row
+            container.setOnClickListener(new View.OnClickListener() {
+
+                //2. Navigate to new activity on tap
+
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("title", movie.getTitle());
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
